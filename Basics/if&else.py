55cde70c4b42d8a259b7d0e538248a845fcfb9e6
Nora <yang_hope@163.com>
2017-07-18 11:33:48 +0800

    Create if&else.py

---
+++ Basics/if&else.py
@@ -0,0 +1,10 @@
+age=3
+if age >= 18:
+    print('your age is', age)
+    print('adult')
+elif age >=6:
+	print('your age is', age)
+	print('teenager')
+else:
+	print('your age is', age)
+	print('kid')
