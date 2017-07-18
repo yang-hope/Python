75ee045d88fbda861357c797042380e1516993dc
Nora <yang_hope@163.com>
2017-07-18 14:50:34 +0800

    Create list.py

---
+++ Basics/list.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+
+L=[
+  ['apple', 'google', 'microsoft'],
+  ['java', 'python', 'ruby', 'php'],
+  ['adam', 'bart', 'lisa']
+]
+#打印apple
+print(L[0][0])
+#打印python
+print(L[1][1])
+#打印lisa
+print(L[2][2])
