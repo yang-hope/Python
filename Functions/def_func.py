845441df9b602d8b32ff43f60acc0428699aef5c
Nora <yang_hope@163.com>
2017-07-24 13:46:08 +0800

    Create def_func.py

---
+++ Functions/def_func.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+
+def my_abs(x):
+	if not isinstance(x, (int, float)):
+		raise TypeError ('bad operand type')
+	if x >= 0:
+		return x
+	else:
+		return -x
+    
+s = my_abs(-7)
+print (s)
