2cb30d36957df23abcd563b6fb379683b006f0d0
Nora <yang_hope@163.com>
2017-08-15 14:24:44 +0800

    Update yanghui_triangles.py

--- Advanced/yanghui_triangles.py
+++ Advanced/yanghui_triangles.py
@@ -5,6 +5,7 @@ def triangles():
 	while True:
 		yield b
 		b = [1] + [b[i] + b[i+1] for i in range(len(b)-1)] + [1]
+		# b = [sum(i) for i in zip([0]+b, b+[0])]
 n = 0
 for t in triangles():
 	print(t)
