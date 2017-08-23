c460d165b846d2f2369de9473280eb1d253dc629
Nora <yang_hope@163.com>
2017-08-23 14:37:56 +0800

    Update map.py

--- Advanced/map.py
+++ Advanced/map.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+#map
 
 def normalize(name):
 	return str(name).title()
@@ -6,3 +7,16 @@ def normalize(name):
 L1 = ['adam', 'LISA', 'barT']
 L2 = list(map(normalize, L1))
 print(L2)
+
+#reduce
+from functools import reduce
+
+def prod(L):
+	def multi(x,y):
+		return x*y
+
+	return reduce(multi, L)
+
+print('3 * 5 * 7 * 9 =', prod([3, 5, 7, 9]))
+
+#map&reduce
